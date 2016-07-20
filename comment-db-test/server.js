acd679d02a6067ba932c52c1143d769ffd5d961d
Celeste Glavin <cmugla@gmail.com>
2016-07-20 16:19:53 -0400

    create codes csv file to mongoimport

---
+++ comment-db-test/server.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const express         = require('express')
+const logger          = require('morgan')
+const app             = express()
+const path            = require('path')
+const bodyParser      = require('body-parser')
+const mongoDB         = require('mongodb')
+
+const PORT            = process.env.PORT || process.argv[2] || 3000
+
+// set up logging so that we can see what's happening
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname,'public')));
+
+// set up ejs to render the views
+app.set('view engine', 'ejs')
+
+// set up server
+app.listen(PORT, function(){
+  console.log("server up and running on port ", PORT)
+})
