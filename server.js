9685ae839142af028b33eb4a806b290cf1ce001e
John Sigmon <johnasigmon@gmail.com>
2016-07-21 10:37:07 -0400

    return json data

--- server.js
+++ server.js
@@ -19,63 +19,21 @@ app.use(logger('dev'));
 // bobbyParser.jason
 app.use(bobbyParser.urlencoded({extended: false}));
 
-// app.get('/', app.get('/api',(req,res)=>{
+app.get('/',(req,res)=>{
 
   request({
     headers: {
       "X-Auth-API-Key": "hq4mup65jf8zc6awqws4puut"
     },
-    url:'https://api.tugroup.com/v1/travelsafe/countries/BO',
+    url:'https://api.tugroup.com/v1/travelsafe/countries/LA',
     method:'get',
-    qs:{
-
-      format:"json"
-    },
     json:true
-  },(err,res,body)=>{
+  },(err,response,body)=>{
     if(err) throw err;
 
+      console.log(body)
+      res.results = body;
+      res.json(res.results)
 
-
-      res.apiResults = body.businesses;
-      console.log(res.apiResults)
   })
-
-
-
-/* Test this route! Load localhost:3000/ and use $.get('/api')
-*  This is hard-coding in query parameters though, if our AJAX call sends query parameters, we access them with req.query!
-*  In that case, we can just say qs: req.query
-*/
-
-
-// app.get('/api',(req,res)=>{
-
-//   request({
-//     headers: {
-//       "X-Auth-API-Key": "hq4mup65jf8zc6awqws4puut"
-//     },
-//     url:'https://api.tugroup.com/v1/travelsafe/countries/LA',
-//     method:'get',
-//     qs:{
-
-//       format:"json"
-//     },
-//     json:true
-//   },(err,res,body)=>{
-//     if(err) throw err;
-
-
-//       console.log(body)
-//       res.results = body.businesses;
-//       next()
-//   })
-
-
-
-/* wunderground.com/apikey/v2/ny/newyork
-* wunderground_api_key
-
-* `wunderground.com/${process.env.wunderground_api_key}/v2/ny/newyork`
-
-*/
+});
