efd47e2206d56d0141bbbc8b8f02affdc88fe82c
aleksander kutsenkov <ligalizer8@gmail.com>
2016-07-22 09:46:08 -0400

    las

--- react-box-input/src/helpers/AjaxAdapter.js
+++ react-box-input/src/helpers/AjaxAdapter.js
@@ -3,11 +3,9 @@ export default class AjaxAdapter{
   constructor(fetch){
     if(!fetch) throw "We need the Fetch library to make this work, bru.";
   }
-
-   handleCountryList() {
-
-    // let self = this
-     return fetch('/dropd')
+    handleCountryList() {
+     // let self = this
+    return fetch('/dropd')
     .then(r => r.json())
 
 }
