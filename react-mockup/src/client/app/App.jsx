ad2fdd6d2e87eba5e0897dfbb043cf40d4bd02da
erin cherney <erin.t.cherney@gmail.com>
2016-07-23 15:41:40 -0400

    cleared ajax

--- react-mockup/src/client/app/App.jsx
+++ react-mockup/src/client/app/App.jsx
@@ -6,11 +6,6 @@ import ReactDOM         from 'react-dom'
 import CreateUser       from './CreateUser.jsx'
 import Login            from './Login.jsx'
 
-import AjaxAdapter      from '../helpers/AjaxAdapter.js'
-
-// Init the Ajax Adapter, give it the fetch library
-const ajax = new AjaxAdapter(fetch);
-
 // create a React Component called _App_
 export default class App extends React.Component {
 
