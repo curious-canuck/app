c0bf50afd045025af0454d862a42bc5e992b9af9
erin cherney <erin.t.cherney@gmail.com>
2016-07-22 20:20:09 -0400

    auth work done

---
+++ react-mockup/src/client/app/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Profile extends React.Component {
+  render(){
+    return(
+      <div id="user" className="jumbotron">
+        <h2>THIS USER INFO</h2>
+        <button>Country</button>
+        <button>Country</button>
+        <button>Country</button>
+      </div>
+    )
+  }
+
+}
