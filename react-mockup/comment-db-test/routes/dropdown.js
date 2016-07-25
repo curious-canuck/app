480043fe07ad781563ea274134f714973b70c336
erin cherney <erin.t.cherney@gmail.com>
2016-07-24 21:19:14 -0400

    cleared out files

--- react-mockup/comment-db-test/routes/dropdown.js
+++
@@ -1,9 +0,0 @@
-const dropdownRouter  = require('express').Router()
-const ccDB            = require('../models/countryNames')
-
-dropdownRouter.get('/', ccDB.getAllCountryNames, (req,res)=>{
-  res.json(res.codes)
-})
-
-module.exports = dropdownRouter;
-
--- routes/travelAdvisory.js
+++
@@ -1,8 +0,0 @@
-const apiRouter       = require('express').Router();
-const travelAPI       = require('../models/travelAdvisory');
-
-  apiRouter.get('/:code', travelAPI.getData, (req,res)=>{
-    res.json(res.results)
-  });
-
-module.exports = apiRouter
