10c0e1413a19f63dedd9d3630c47a2722717c93e
John Sigmon <johnasigmon@gmail.com>
2016-08-23 12:58:14 -0400

    add a different webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,31 @@
 'use strict'
 const webpack           = require('webpack');
 const path              = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const BUILD_DIR         = path.resolve(__dirname, 'dist');
 const APP_DIR           = path.resolve(__dirname, 'src/client/app');
 
+webpack({
+    // configuration
+}, function(err, stats) {
+    if(err)
+        return handleFatalError(err);
+    var jsonStats = stats.toJson();
+    if(jsonStats.errors.length > 0)
+        return handleSoftErrors(jsonStats.errors);
+    if(jsonStats.warnings.length > 0)
+        handleWarnings(jsonStats.warnings);
+    successfullyCompiled();
+});
 
-module.exports = {
-  entry: `${APP_DIR}/main.jsx`,
+const config = {
+  entry: `${APP_DIR}/main.js`,
   output: {
     path: BUILD_DIR,
     filename: '/js/[name].js',
   },
+
   cache: true,
   debug: true,
   devtool: 'eval-source-map',
@@ -21,31 +33,22 @@ module.exports = {
     colors: true,
     reasons: true
   },
+
   plugins: [
-    /*new webpack.optimize.UglifyJsPlugin({
-      compress:{
-        warnings: true
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
       }
-    }),*/
-    new webpack.optimize.CommonsChunkPlugin('/js/common.js'),
-    new HtmlWebpackPlugin({
-      title: 'Curious Canuck App',
-      xhtml: true,
-      inject: false,
-      template: require('html-webpack-template'),
-      appMountId: 'container'
     }),
     new ExtractTextPlugin('/css/[name].css', {
-      allChunks: true
+      allChunks: false
     })
   ],
-
   module : {
-    // include: path.join(__dirname, 'src'),
     loaders: [
       { test: /\.css$/,  loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
       { test: /\.(png|gif|jpg)$/,  loader: 'file-loader?name=/images/[name].[ext]' },
-      { test: /\.jsx?$/, loader: 'babel'       },
+      { test: /\.ico$/,  loader: 'file-loader?name=/[name].[ext]' },
       {
         test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
         loader: 'url-loader?limit=100&mimetype=application/font-woff&name=/fonts/[name].[ext]'
@@ -60,8 +63,11 @@ module.exports = {
       },
       {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=100&mimetype=image/svg+xml&name=/fonts/[name].[ext]'
+        loader: 'url-loader?limit=100&mimetype=image/svg+xml&name=/images/[name].[ext]'
       }
     ]
-  }
+  },
 };
+
+
+ module.exports = config;
